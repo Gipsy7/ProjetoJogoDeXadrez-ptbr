81f390c3b1e116f120787c7d8d7564dd8aa36ee0
GipsyDangerM3 <mikael_dev@hotmail.com>
2022-02-14 11:04:36 -0300

    Pequenas alteracoes na classe Peao

--- Xadrez-console/Xadrez/Pecas/Peao.cs
+++ Xadrez-console/Xadrez/Pecas/Peao.cs
@@ -39,17 +39,20 @@ namespace Xadrez
                 {
                     movimentos[posicao.Linha, posicao.Coluna] = true;
                 }
+
                 posicao.DefinirValores(Posicao.Linha - 2, Posicao.Coluna);
                 Posicao p2 = new Posicao(Posicao.Linha - 1, Posicao.Coluna);
                 if (Tabuleiro.PosicaoValida(posicao) && Livre(posicao) && Tabuleiro.PosicaoValida(p2) && Livre(p2) && QteMovimentos == 0)
                 {
                     movimentos[posicao.Linha, posicao.Coluna] = true;
                 }
+
                 posicao.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
                 if (Tabuleiro.PosicaoValida(posicao) && ExisteInimigo(posicao))
                 {
                     movimentos[posicao.Linha, posicao.Coluna] = true;
                 }
+
                 posicao.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
                 if (Tabuleiro.PosicaoValida(posicao) && ExisteInimigo(posicao))
                 {
@@ -79,17 +82,20 @@ namespace Xadrez
                 {
                     movimentos[posicao.Linha, posicao.Coluna] = true;
                 }
+
                 posicao.DefinirValores(Posicao.Linha + 2, Posicao.Coluna);
                 Posicao p2 = new Posicao(Posicao.Linha + 1, Posicao.Coluna);
                 if (Tabuleiro.PosicaoValida(posicao) && Livre(posicao) && Tabuleiro.PosicaoValida(p2) && Livre(p2) && QteMovimentos == 0)
                 {
                     movimentos[posicao.Linha, posicao.Coluna] = true;
                 }
+
                 posicao.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
                 if (Tabuleiro.PosicaoValida(posicao) && ExisteInimigo(posicao))
                 {
                     movimentos[posicao.Linha, posicao.Coluna] = true;
                 }
+
                 posicao.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
                 if (Tabuleiro.PosicaoValida(posicao) && ExisteInimigo(posicao))
                 {
